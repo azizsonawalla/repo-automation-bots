0f9e6afd8ac3c6156d958387a225fb5d57c9cfb7
Aziz Sonawalla <asonawalla@google.com>
2020-07-13 16:57:05 -0400

    feat(gcf-util): implement logging overrides for issues endpoints

--- packages/gcf-utils/src/octokit-logger.ts
+++ packages/gcf-utils/src/octokit-logger.ts
@@ -1,13 +1,27 @@
 import {logger, GCFLogger} from './gcf-utils';
 import {Octokit} from '@octokit/rest';
 
-enum GitHubActionType {}
+enum GitHubActionType {
+  ADD_LABEL,
+  REMOVE_LABEL,
+  ADD_COMMENT,
+  CREATE_ISSUE_LABEL,
+  UPDATE_ISSUE_LABEL,
+  UNKNOWN,
+  DELETE_ISSUE_LABEL,
+  UPDATE_ISSUE,
+  CREATE_ISSUE
+}
 
-enum GitHubObjectType {}
+enum GitHubObjectType {
+  ISSUE,
+  UNKNOWN,
+}
 
 enum GitHubOwnerType {
-  User = 'USER',
-  Org = 'ORG',
+  USER,
+  ORG,
+  UNKNOWN,
 }
 
 interface GitHubAction {
@@ -16,7 +30,7 @@ interface GitHubAction {
     value: string;
     destination_object?: {
       object_type: GitHubObjectType;
-      object_id: string;
+      object_id: string | number;
     };
     destination_repo: {
       repo_name: string;
@@ -30,8 +44,60 @@ interface GitHubAction {
 type RequestOptions = Octokit.RequestOptions;
 type Response<T> = Promise<Octokit.Response<T>>;
 
-module.exports = (octokit: Octokit, options: {customLogger?: GCFLogger}) => {
+function logGithubIssueAction(
+  type?: GitHubActionType,
+  value?: string,
+  params?: {issue_number?: number; repo: string; owner: string}
+) {
+  const hasIssueNumber: boolean =
+    params?.hasOwnProperty('issue_number') || false;
+  logGithubAction(
+    type,
+    value,
+    hasIssueNumber ? GitHubObjectType.ISSUE : undefined,
+    hasIssueNumber ? params?.issue_number : undefined,
+    params?.repo,
+    params?.owner,
+    GitHubOwnerType.UNKNOWN
+  );
+}
 
+function logGithubAction(
+  type?: GitHubActionType,
+  value?: string,
+  dstObjType?: GitHubObjectType,
+  dstObjId?: string | number,
+  repoName?: string,
+  repoOwner?: string,
+  repoOwnerType?: GitHubOwnerType
+) {
+  const general = {
+    type: type || GitHubActionType.UNKNOWN,
+    value: value || 'UNKNOWN',
+    destination_repo: {
+      repo_name: repoName || 'UNKNOWN',
+      owner: repoOwner || 'UNKNOWN',
+      owner_type: repoOwnerType || GitHubOwnerType.UNKNOWN,
+    },
+  };
+
+  const destination_object =
+    dstObjType || dstObjId
+      ? {
+          destination_object: {
+            object_type: dstObjType || GitHubObjectType.UNKNOWN,
+            object_id: dstObjId || 'UNKNOWN',
+          },
+        }
+      : {};
+
+  const githubAction: GitHubAction = {
+    action: {...general, ...destination_object},
+  };
+  logger.metric(githubAction);
+}
+
+module.exports = (octokit: Octokit, options: {customLogger?: GCFLogger}) => {
   const octoLogger: GCFLogger = options.customLogger || logger;
 
   // TODO: add comment about octokit version
@@ -40,33 +106,89 @@ module.exports = (octokit: Octokit, options: {customLogger?: GCFLogger}) => {
       addLabels: (
         params?: RequestOptions & Octokit.IssuesAddLabelsParams
       ): Response<Octokit.IssuesAddLabelsResponse> => {
-        // TODO: log
+        logGithubIssueAction(
+          GitHubActionType.ADD_LABEL,
+          params?.labels.join(', '),
+          params
+        );
         return octokit.issues.addLabels(params);
       },
       createComment: (
         params?: RequestOptions & Octokit.IssuesCreateCommentParams
       ): Response<Octokit.IssuesCreateCommentResponse> => {
-        // TODO: log
+        logGithubIssueAction(
+          GitHubActionType.ADD_COMMENT,
+          params?.body,
+          params
+        );
         return octokit.issues.createComment(params);
       },
-      // createLabel: (
-      //   params?: RequestOptions & Octokit.IssuesCreateLabelParams
-      // ): Response<Octokit.IssuesCreateLabelResponse> => {},
-      // removeLabel: (
-      //   params?: RequestOptions & Octokit.IssuesRemoveLabelParams
-      // ): Response<Octokit.IssuesRemoveLabelResponse> => {},
-      // updateLabel: (
-      //   params?: RequestOptions & Octokit.IssuesUpdateLabelParams
-      // ): Response<Octokit.IssuesUpdateLabelResponse> => {},
-      // deleteLabel: (
-      //   params?: RequestOptions & Octokit.IssuesDeleteLabelParams
-      // ): Promise<Octokit.AnyResponse> => {},
-      // update: (
-      //   params?: RequestOptions & Octokit.ChecksUpdateParams
-      // ): Response<Octokit.ChecksUpdateResponse> => {},
-      // create: (
-      //   params?: RequestOptions & Octokit.GistsCreateParams
-      // ): Response<Octokit.GistsCreateResponse> => {},
+
+      createLabel: (
+        params?: RequestOptions & Octokit.IssuesCreateLabelParams
+      ): Response<Octokit.IssuesCreateLabelResponse> => {
+        logGithubIssueAction(
+          GitHubActionType.CREATE_ISSUE_LABEL,
+          params?.name,
+          params
+        );
+        return octokit.issues.createLabel(params);
+      },
+
+      removeLabel: (
+        params?: RequestOptions & Octokit.IssuesRemoveLabelParams
+      ): Response<Octokit.IssuesRemoveLabelResponse> => {
+        logGithubIssueAction(
+          GitHubActionType.REMOVE_LABEL,
+          params?.name,
+          params
+        );
+        return octokit.issues.removeLabel(params);
+      },
+
+      updateLabel: (
+        params?: RequestOptions & Octokit.IssuesUpdateLabelParams
+      ): Response<Octokit.IssuesUpdateLabelResponse> => {
+        logGithubIssueAction(
+          GitHubActionType.UPDATE_ISSUE_LABEL,
+          `${params?.current_name} to ${params?.name}`,
+          params
+        );
+        return octokit.issues.updateLabel(params);
+      },
+
+      deleteLabel: (
+        params?: RequestOptions & Octokit.IssuesDeleteLabelParams
+      ): Promise<Octokit.AnyResponse> => {
+        logGithubIssueAction(
+          GitHubActionType.DELETE_ISSUE_LABEL,
+          params?.name,
+          params
+        );
+        return octokit.issues.deleteLabel(params);
+      },
+
+      update: (
+        params?: RequestOptions & Octokit.IssuesUpdateParams
+      ): Response<Octokit.IssuesUpdateResponse> => {
+        logGithubIssueAction(
+          GitHubActionType.UPDATE_ISSUE,
+          "",
+          params
+        );
+        return octokit.issues.update(params);
+      },
+
+      create: (
+        params?: RequestOptions & Octokit.IssuesCreateParams
+      ): Response<Octokit.IssuesCreateResponse> => {
+        logGithubIssueAction(
+          GitHubActionType.CREATE_ISSUE,
+          "",
+          params
+        );
+        return octokit.issues.create(params);
+      },
     },
     pulls: {
       dismissReview: () => {},
